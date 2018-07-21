17b88e3a9ffd34caba7c70e6bc4c796a0021e015
Pedro Perrone <psperrone@hotmail.com>
2018-07-20 22:58:25 -0300

    Create Item class

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  Dir['lib/*.rb'].each { |f| require_relative "../#{f}" }
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
